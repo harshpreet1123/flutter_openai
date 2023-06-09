d50038dda3b837374959c5a31df3bcde19a05217
Harshpreet Singh <94123019+harshpreet1123@users.noreply.github.com>
2023-06-09 19:25:32 +0530

    Update secrets.dart

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,4 +1,1 @@
-const String OpenAIApiKey =
-    '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';
-
-const String RapidApiKey = '57cb782e1cmshbea1453293842b4p1fe663jsn31abf9086802';
+const String RapidApiKey = '';
